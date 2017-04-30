01de188b54d5a78e4477d2d7f5b3e5bab7c28ebb
Ravi <Ravi@powercosts.com>
2017-04-30 09:40:47 -0500

    Data Conversion

--- DataConversion/src/DataConversion.java
+++ DataConversion/src/DataConversion.java
@@ -9,15 +9,18 @@ public class DataConversion
 		System.out.println("Int - " + (3+5 + 8)/3); 
 //		Value will be a double
 		System.out.println("Double - " + (3+5 + 8)/3.0);
-		
+//		3 is a double
 		double volume = 4/3 * Math.PI * 10*10*10;
+//		3.0 is a double
 		double real_volume = 4/3.0 * Math.PI * 10*10*10;
-		System.out.println("The volume of a sphere with radius 10 is: "+ volume);
-		System.out.println("The real volume of a sphere with a radius of 10 is: "+
-		        real_volume);
-		double fahrenheit = 500;
+		System.out.println("The volume of a sphere with radius 10 is: " + volume);
+		System.out.println("The real volume of a sphere with a radius of 10 is: " + real_volume);
+		double fahrenheit = 66666;
+//		This will always be 0 because 5 divided by 9 as an int is always 0
+		double intCelsius = (fahrenheit - 32) * (5/9);
 		double celsius = (fahrenheit - 32) * (5/9.0);
 		System.out.println("Celsius is: "+ celsius);
+		System.out.println("Celsius is: "+ intCelsius);
 	}
 
 }
